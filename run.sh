8e2a69e0cc195739c72b79f3d1f7b702ae7b2dcd
Stéphane Cottin <stephane.cottin@vixns.com>
2015-07-07 19:36:19 +0200

    add JAR_TYPE env var

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ if [ -z "${JAR_URL}" ]; then
   echo "JAR_URL env var required" > /dev/stderr
   exit 1
 fi
-curl -s -k -o /app.jar "${JAR_URL}"
-exec java $JAVA_OPTIONS -jar /app.jar $JAR_OPTIONS
+[ -n "${JAR_TYPE}" ] || JAR_TYPE="jar"
+curl -s -k -o /app.${JAR_TYPE} "${JAR_URL}"
+exec java $JAVA_OPTIONS -jar /app.${JAR_TYPE} $JAR_OPTIONS
